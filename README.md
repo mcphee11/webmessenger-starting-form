55f1beb75b8377d51f183e9901de218f4f35192a
Matt <mcphee11@gmail.com>
2023-06-01 14:38:03 +1000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # webmessenger-starting-form
 Enables a custom launcher and input form to gather use information before starting the converstaion.
+
+Many people I talk to would like to have an "input form" where users can enter in custom details about their interaction so it can be used for routing purposes as well as flow DataActions to look into 3rd part Dbs etc. While in WebMessaging this is normally done either through data collected in the website using GPE or conversationally through the messenger. But sometime people prefer a more traditional approach to startign a conversation. For this I have created a custom [Launcher](https://developer.genesys.cloud/commdigital/digital/webmessaging/messengersdk/pluginExamples#build-your-own-messenger-launcher) but instead of showing the convesation or homem screen first if there is no existing conversation I am showing an input form.
+
+This data is then sent into Genesys Cloud through the [DataPlugin](https://developer.genesys.cloud/commdigital/digital/webmessaging/messengersdk/SDKCommandsEvents/databasePlugin#database-set) which is then able to be accessed inside architect.
+
+Input User Interface:
+![](/docs/images/inputForm.png?raw=true)
+
+Getting data in the flow:
+![](/docs/images/architect.png?raw=true)
+
+To do this you simply need to have your WebMessenger Deployment setup "Hide" the default launcher as well as not using the "Home" screen. If you must use the home screen you will need to change some of the code to suite that use case.
+
+![](/docs/images/deploymentConfig.png?raw=true)
+
+First you will need to update the
+
+```
+const deploymentId = 'ENTER_YOUR_DEPLOYMENTID'
+```
+with your webmessenger deploymentId. Optionally you can also set the HEX color code to match the widget. I ahve it set to a default of black or #000000
+```
+const hexColor = '#000000'
+```
+
+To add this script to your existing deployment on the webpage you will need to add it as a script tag in the header of the page just below the default WebMessager snippet.
+
+```
+<script src="inviteform.js" defer></script>
+or if using terser:
+<script src="inviteform.min.js" defer></script>
+```
+
+Ensure you have the "defer" as it needs to laod after the normally snippet as this uses the Genesys SDK to interact with the widget.
+
+  ## TERSER NOTE:
+I have included a VSCode "Task" for minifying and uglifying the js file in the "private" folder and it will output it into the "public folder. This is done in my case through [terser](https://terser.org/) this then gives you a .min.js file to use for best practice in production.
+
+If you are not familuar with VSCode Tasks you can find info [here](https://code.visualstudio.com/docs/editor/tasks) in short they are a GREAT way to build out scripts to automate builds of items rather then manually running them each time.
+
+To manually do this you can run the below script if you ahve terser installed
+
+```
+terser ./private/inviteform.js -c -m  -o ./public/inviteform.min.js
+```
---
+++ private/inviteform.js
@@ -0,0 +1,211 @@
+'use strict'
+
+//Variables to change in your deployment
+const deploymentId = 'ENTER_YOUR_DEPLOYMENTID'  //Your WebMessenger DeploymentId
+const hexColor = '#000000'  //Color theme
+
+function toggleMessenger() {
+  Genesys(
+    'command',
+    'Messenger.open',
+    {},
+    function (o) {
+      closeLauncher()
+      Genesys('command', 'Database.set', {
+        messaging: {
+          customAttributes: {
+            firstName: document.getElementById('fname').value,
+            lastName: document.getElementById('lname').value,
+            email: document.getElementById('email').value,
+            case: document.getElementById('case').value,
+            queueName: document.getElementById('queue').value,
+          },
+        },
+      })
+    },
+    function (o) {
+      Genesys('command', 'Messenger.close')
+    }
+  )
+}
+
+function closeLauncher() {
+  let input = document.getElementById('input')
+  input.hidden = true
+  console.log('Hiding...')
+}
+
+function openLauncher() {
+  let session = JSON.parse(localStorage.getItem('_1c077c42-7a8e-421b-85b1-3a4ae63f39f9:gcmcsessionActive'))
+  let input = document.getElementById('input')
+  console.log(session?.value)
+  if (session?.value) {
+    console.log('Opening Widget...')
+    Genesys(
+      'command',
+      'Messenger.open',
+      {},
+      function (o) {
+        closeLauncher()
+      },
+      function (o) {
+        Genesys('command', 'Messenger.close')
+      }
+    )
+  } else {
+    console.log('showing...')
+    input.hidden = false
+  }
+}
+
+//Create Launcher
+let launcher = document.createElement('button')
+launcher.onclick = function () {
+  openLauncher()
+}
+launcher.style = `cursor: pointer;
+      box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 5px -2px, rgba(0, 0, 0, 0.14) 0px 1px 4px 2px, rgba(0, 0, 0, 0.12) 0px 1px 4px 1px;
+      position: fixed !important;
+      bottom: 30px !important;
+      width: 56px;
+      height: 56px;
+      right: 30px !important;
+      border-radius: 50%;
+      background-color: ${hexColor};
+      z-index: 9999;
+      border: 0px;`
+launcher.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/></svg>`
+document.body.appendChild(launcher)
+
+//Create Input Form
+let input = document.createElement('div')
+let header = document.createElement('div')
+let title = document.createElement('p')
+let minButton = document.createElement('button')
+
+let form = document.createElement('div')
+let fnameL = document.createElement('label')
+let fnameI = document.createElement('input')
+let lnameL = document.createElement('label')
+let lnameI = document.createElement('input')
+let emailL = document.createElement('label')
+let emailI = document.createElement('input')
+let caseL = document.createElement('label')
+let caseI = document.createElement('input')
+let queueL = document.createElement('label')
+let queueS = document.createElement('select')
+let option1 = document.createElement('option')
+let option2 = document.createElement('option')
+let option3 = document.createElement('option')
+let submit = document.createElement('button')
+
+input.id = 'input'
+input.hidden = true
+input.style = `box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 5px -2px, rgba(0, 0, 0, 0.14) 0px 1px 4px 2px, rgba(0, 0, 0, 0.12) 0px 1px 4px 1px;
+      position: fixed !important;
+      bottom: 30px !important;
+      width: 408px;
+      height: 648px;
+      right: 30px !important;
+      background-color: white;
+      z-index: 99999;`
+header.style = `display: inline-flex;
+      background-color: ${hexColor};
+      color: white;
+      font-size: 1.33929rem;
+      line-height: 2.6;
+      font-weight: 400;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'sans-serif';
+      width: 100%;
+      height: 60px;`
+title.style = `margin: 0;
+      padding-left: 25px;`
+title.innerText = 'Please give us some info'
+minButton.style = `position: absolute;
+      width: 50px;
+      right: 8px;
+      top: 15px;
+      cursor: pointer;
+      filter: invert(1);
+      border: 0;
+      background-color: transparent`
+minButton.onclick = function () {
+  closeLauncher()
+}
+minButton.tabIndex = 0
+minButton.ariaLabel = 'Minimize the Messenger'
+minButton.innerHTML = `<svg id="svgid" viewBox="0 0 24 24" style="width: 26px; height: 26px;">
+<title>window-minimize</title>
+<path d="M19 13H5v-2h14v2z"></path>
+</svg>`
+header.appendChild(title)
+header.appendChild(minButton)
+input.appendChild(header)
+
+form.style = `padding: 25px;`
+fnameL.innerText = 'First Name'
+fnameL.style = `font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'sans-serif';`
+fnameI.id = 'fname'
+fnameI.style = `      width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;`
+fnameI.placeholder = 'Your first name..'
+lnameL.innerText = 'Last Name'
+lnameL.style = `font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'sans-serif';`
+lnameI.id = 'lname'
+lnameI.style = `      width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;`
+lnameI.placeholder = 'Your last name..'
+emailL.innerText = 'Email'
+emailL.style = `font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'sans-serif';`
+emailI.id = 'email'
+emailI.style = `      width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;`
+emailI.placeholder = 'Your email address..'
+caseL.innerText = 'Case'
+caseL.style = `font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'sans-serif';`
+caseI.id = 'case'
+caseI.style = `      width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;`
+caseI.placeholder = 'Optional case number..'
+queueL.innerText = 'Queue'
+queueL.style = `font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 'sans-serif';`
+queueS.id = 'queue'
+queueS.style = `    width: 100%;
+padding: 12px 20px;
+margin: 8px 0;
+display: inline-block;
+border: 1px solid #ccc;
+border-radius: 4px;
+box-sizing: border-box;`
+option1.value = 'sales'
+option1.innerText = 'Sales'
+option2.value = 'support'
+option2.innerText = 'Support'
+option3.value = 'general'
+option3.innerText = 'General'
+submit.style = `width: 100%;
+background-color: ${hexColor};
+color: white;
+padding: 14px 20px;
+margin: 100px 0 0 0;
+border: none;
+border-radius: 4px;
+cursor: pointer;`
+submit.innerText = 'Submit'
+submit.onclick = function () {
+  toggleMessenger()
+}
+
+queueS.appendChild(option1)
+queueS.appendChild(option2)
+queueS.appendChild(option3)
+form.appendChild(fnameL)
+form.appendChild(fnameI)
+form.appendChild(lnameL)
+form.appendChild(lnameI)
+form.appendChild(emailL)
+form.appendChild(emailI)
+form.appendChild(caseL)
+form.appendChild(caseI)
+form.appendChild(queueL)
+form.appendChild(queueS)
+form.appendChild(submit)
+input.appendChild(form)
+
+document.body.appendChild(input)
